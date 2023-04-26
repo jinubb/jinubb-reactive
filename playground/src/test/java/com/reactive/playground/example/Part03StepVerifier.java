a12647593ef98e000f38d98d814019137916f2ec
jinubb <53621550+jinubb@users.noreply.github.com>
2023-04-26 10:36:30 +0900

    study : 1~3장 학습

--- playground/src/test/java/com/reactive/playground/example/Part03StepVerifier.java
+++ playground/src/test/java/com/reactive/playground/example/Part03StepVerifier.java
@@ -17,8 +17,11 @@ package com.reactive.playground.example;
  */
 
 import com.reactive.playground.domain.User;
+import java.time.Duration;
 import java.util.function.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 /**
  * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
@@ -32,14 +35,22 @@ public class Part03StepVerifier {
 
     // TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
     void expectFooBarComplete(Flux<String> flux) {
-        fail();
+        StepVerifier.create(flux)
+            .expectNext("foo", "bar")
+            .expectComplete()
+            .verify();
     }
 
 //========================================================================================
 
     // TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
     void expectFooBarError(Flux<String> flux) {
-        fail();
+        StepVerifier.create(flux.error(new RuntimeException()))
+            .expectError(RuntimeException.class)
+            .verify();
+
+        StepVerifier.create(flux).expectNext("foo","bar").expectError(RuntimeException.class).verify();
+
     }
 
 //========================================================================================
@@ -47,14 +58,20 @@ public class Part03StepVerifier {
     // TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
     // and another one with "jpinkman" then completes successfully.
     void expectSkylerJesseComplete(Flux<User> flux) {
-        fail();
+        StepVerifier.create(flux)
+            .assertNext(user -> user.getUsername().equals("swhite"))
+            .assertNext(user -> user.getUsername().equals("jpinkman"))
+            .expectComplete()
+            .verify();
     }
 
 //========================================================================================
 
     // TODO Expect 10 elements then complete and notice how long the test takes.
     void expect10Elements(Flux<Long> flux) {
-        fail();
+        StepVerifier.create(Flux.interval(Duration.ofSeconds(5)))
+            .thenAwait(Duration.ofMillis(1000l))            .expectTimeout(Duration.ofSeconds(1))
+            .verify();
     }
 
 //========================================================================================
